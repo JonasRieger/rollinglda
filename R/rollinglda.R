e5d1f1adb012ce5e3e8f01f2a1ed7e942bfce29d
Jonas Rieger <jonas.rieger@tu-dortmund.de>
2021-08-09 18:14:19 +0200

    ideas for memory parameter

--- R/rollinglda.R
+++ R/rollinglda.R
@@ -3,19 +3,55 @@
 # texts muss named list of tokenized texts sein!!!
 # dates muss gleiche Länge und Sortierung (wie texts) haben, oder benannt sein!
 
-rollinglda = function(){
+#getDocs muss noch geschrieben werden
+
+rollinglda_update = function(x, texts, dates, memory, param = getParam(x)){
+  if (is.null(names(dates))) names(dates) = names(docs)
+
+  assert_character(as.character(memory), any.missing = FALSE, len = 1)
+
+  if (!is.Date(memory)){
+    memory.try = try(as.Date(memory), silent = TRUE)
+    if (inherits(memory.try, "try-error")){
+      number.memory = 1L
+      if (grepl("[0-9]", memory))
+        number.memory = as.integer(gsub("([0-9]+).*", "\\1", memory))
+      unit.memory = gsub("([0-9]+)(.*)", "\\2", memory)
+      if (unit.memory == ""){
+        memory = sort(getDates(x), decreasing = TRUE)[number.memory]
+      }
+      floor_date(min(dates), unit.memory)
+      # TODO!!!
+    }else memory = memory.try
+  }
 
 
-  step.new = rollinglda_one_step(lda, docs, texts,
-    vocab.abs = vocab.abs, vocab.rel = vocab.rel, vocab.fallback = vocab.fallback,
-    doc.abs = doc.abs)
+  #cuts, memory
 
-  # TODO!
-  #if (!keep.docs){ ...
-  #docs = docs[]
-  #dates = dates[]
-  #chunks = data.table() ...
-  res = list(lda = lda, docs = docs, dates = dates, chunks = chunks,
+  #bestimme memory = DATE !
+
+  dates.memory = getDates(x)
+  id.memory = names(dates.memory[dates.memory >= memory])
+  docs.memory = getDocs(x, id.memory)
+
+
+  step.new = rollinglda_one_step(
+    lda = getLDA(x),
+    docs = docs.memory,
+    texts = texts,
+    vocab.abs = vocab.abs,
+    vocab.rel = vocab.rel,
+    vocab.fallback = vocab.fallback,
+    doc.abs = doc.abs
+  )
+
+  if (!keep.docs){
+    docs = step.new$docs
+    dates = c(getDates(x, id.memory), dates)
+  }
+  # TODO!!
+  #chunks = data.table() ... # merge, füllen mit NAs
+  res = list(lda = step.new$lda, docs = docs, dates = dates, chunks = chunks,
     param = list(vocab.abs = vocab.abs, vocab.rel = vocab.rel,
       vocab.fallback = vocab.fallback, doc.abs = doc.abs))
   class(res) = "RollingLDA"
@@ -25,8 +61,7 @@ rollinglda = function(){
 
 # internal (docs ist hier schon korrekt reduziert - docs = docs.memory!!)
 rollinglda_one_step = function(lda, docs, texts,
-  vocab.abs = 5, vocab.rel = 0, vocab.fallback = 100, doc.abs = 0,
-  compute.topics = FALSE){
+  vocab.abs = 5, vocab.rel = 0, vocab.fallback = 100, doc.abs = 0){
 
   # vocab bestimmen?
   # assignments bestimmen (memory-begin bis aktuelles-chunk-ende)
@@ -59,16 +94,12 @@ rollinglda_one_step = function(lda, docs, texts,
     eta = getEta(lda),
     num.iterations = getNum.iterations(lda)
   )
-  if(compute.topics)
-    topics = compute_topics_matrix_from_assignments(
-      append(getAssignments(lda), res$assignments), ALL_docs, getK(lda), vocab)
-  else topics = NULL
   list(
     lda = LDA(
       assignments = append(getAssignments(lda), res$assignments),
-      topics = topics,
       document_sums = cbind(getDocument_sums(lda), res$document_sums),
-      param = list(K = K, alpha = alpha, eta = eta, num.iterations = num.iterations)),
+      param = list(K = K, alpha = alpha, eta = eta, num.iterations = num.iterations)
+    ),
     docs = docs,
     n.docs.new = n.docs.new,
     n.docs.deleted = n.docs.deleted,
@@ -86,10 +117,12 @@ rollinglda_one_step_fitting = function(assignments, docs, vocab, n.init,
       assignments = assignments,
       topics = topics,
       topic_sums = matrix(as.integer(rowSums(topics)))),
-    n.init = n.init)
+    n.init = n.init
+  )
   list(
     assignments = res$assignments[(n.init+1):length(docs)],
-    document_sums = res$document_sums[,(n.init+1):length(docs)])
+    document_sums = res$document_sums[,(n.init+1):length(docs)]
+  )
 }
 
 # internal
